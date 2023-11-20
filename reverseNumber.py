01c23fc1ffaca8cb4fc9959df289e82f1610cf9b
Keyurrr14 <keyurrathod9920@gmail.com>
2023-11-20 15:23:13 +0530

    New leetcode problem

---
+++ reverseNumber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        stringRev = str(abs(x))
+        stringRev = stringRev.strip()
+        stringRev = stringRev[::-1]
+        
+        result = int(stringRev)
+        
+        if result >= 2** 31 -1 or result <= -2** 31:
+            return 0
+        elif x < 0:
+            return -1 * result
+        else:
+            return result
