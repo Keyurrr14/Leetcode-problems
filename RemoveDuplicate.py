7afdad12d9d8ed72d9a554bd5ac228a4de4dc341
Keyurrr14 <keyurrathod9920@gmail.com>
2024-04-11 12:22:17 +0530

    11-04-2024 ✅

---
+++ RemoveDuplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 1
+
+        for right in range(1, len(nums)):
+            if nums[right] != nums[right-1]:
+                nums[left] = nums[right]
+                left += 1
+
+        return left
+    
+# Initialze the left and right to 1 because 0th index element is not gonna change
+# right iterates through the entire nums list
+# we compare the current right pointer to the previous one, and if they are not equal meaining they are unique, and put them in the position of the left pointer. Increment the left pointer
+# return left as it indiacted the number of duplicates
--- reverseNumber.py
+++ reverseNumber.py
@@ -12,3 +12,4 @@ class Solution:
             return -1 * result
         else:
             return result
+        
