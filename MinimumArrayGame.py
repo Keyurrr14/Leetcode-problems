bc9f200d8e4a0d97fee43dee16b8f37cb9b9a805
Keyurrr14 <keyurrathod9920@gmail.com>
2024-08-31 23:16:23 +0530

    31-08-2024✅

---
+++ MinimumArrayGame.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        arr = []
+        while len(nums) > 0:
+            alice = nums.pop(0) 
+            bob = nums.pop(0)
+            arr.append(bob) 
+            arr.append(alice) 
+
+        return arr
