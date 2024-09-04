c489f0c0381fa22f05693b020d44cb92db7e47e0
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-04 21:42:18 +0530

    04-09-2024✅

---
+++ ContainsDuplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numbers = set()
+        for i in nums:
+            if i in numbers:
+                return True
+            else:
+                numbers.add(i)
+        return False
