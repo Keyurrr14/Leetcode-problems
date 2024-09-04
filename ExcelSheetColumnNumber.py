4d1f52a15aace45f97a4a603e7105db62d1e5732
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-04 22:14:23 +0530

    04-09-2024✅

---
+++ ExcelSheetColumnNumber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        hashMap={"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26}
+        s = columnTitle
+        n = len(s)
+
+        res = 0
+        multiplier = 1
+        for i in range(n-1, -1, -1):
+            res += hashMap[s[i]] * multiplier
+            multiplier *= 26
+        return res
+    
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        count = 0
+        for c in columnTitle:
+            currentVal = ord(c) - ord('A') + 1
+            count = count * 26 + currentVal
+
+        return count
