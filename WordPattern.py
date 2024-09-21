0da6ffa386e11195bbba62fc25052230a6b45284
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-21 22:09:49 +0530

    21-09-2024✅

---
+++ WordPattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        if len(words) != len(pattern):
+            return False
+
+        charToWord = {}
+        wordToChar = {}
+        for char, word in zip(pattern, words):
+            if char in charToWord and charToWord[char] != word:
+                return False
+            if word in wordToChar and wordToChar[word] != char:
+                return False
+            charToWord[char] = word
+            wordToChar[word] = char
+        
+        return True
