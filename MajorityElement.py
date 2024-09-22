4ce5203be82045b350c75136061a892ee2364d84
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-22 22:38:04 +0530

    22-09-2024✅

--- MajorityElement.py
+++ MajorityElement.py
@@ -3,3 +3,14 @@ class Solution:
         nums.sort()
         n = len(nums)
         return nums[n//2]
+    
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if candidate == num else -1
+        return candidate
