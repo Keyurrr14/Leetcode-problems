e3dbc887a31df1a4122305a9d451913cb13ea01f
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-25 20:29:23 +0530

    25-09-2024✅

--- DisappeardFromArray.py
+++ DisappeardFromArray.py
@@ -9,3 +9,16 @@ class Solution:
 
         return missing
     
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        duplicateNums = [0] * (len(nums) + 1)
+        res = []
+        for i in nums:
+            duplicateNums[i] += 1
+        for i in range(1, len(nums) + 1):
+            if duplicateNums[i] == 0:
+                res.append(i)
+            
+        return res
+        
