5fd9d55a2f7ad6408bb4432a990757f9d9ea63bd
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-26 22:42:15 +0530

    26-09-2024✅

--- FIndDifference.py
+++ FIndDifference.py
@@ -5,3 +5,9 @@ class Solution:
                 s = s.replace(char, " ", 1)
             else:
                 return char
+            
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        sumS = sum(ord(i) for i in s)
+        sumT = sum(ord(j) for j in t)
+        return chr(sumT - sumS)
