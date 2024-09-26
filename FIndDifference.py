21e650d43d381e10708b69b6c9c6a705ae13f745
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-26 22:40:23 +0530

    26-09-2024✅

---
+++ FIndDifference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for char in t:
+            if char in s:
+                s = s.replace(char, " ", 1)
+            else:
+                return char
