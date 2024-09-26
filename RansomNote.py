5daf08d0e9b0ea3accd67ad1da5a949022185f0a
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-26 22:34:42 +0530

    26-09-2024✅

---
+++ RansomNote.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazineMap = {}
+        for char in magazine:
+            if char in magazineMap:
+                magazineMap[char] += 1
+            else:
+                magazineMap[char] = 1
+        
+        for char in ransomNote:
+            if char not in magazineMap:
+                return False
+            elif magazineMap[char] == 1:
+                del magazineMap[char]
+            else:
+                magazineMap[char] -= 1
+        
+        return True
