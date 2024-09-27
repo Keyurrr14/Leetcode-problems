381eb3e1eda928b519de2679d3a06c9dbea8c4a4
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-27 21:45:39 +0530

    27-09-2024✅

--- PerfectSquare.py
+++ PerfectSquare.py
@@ -1,3 +1,21 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         return int(num ** 0.5) * int(num ** 0.5) == num
+    
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 1
+        right = num
+
+        while left <= right:
+            mid = (left + right) // 2
+            midSquared = mid * mid
+
+            if midSquared == num:
+                return True
+            elif midSquared < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+            
+        return False
