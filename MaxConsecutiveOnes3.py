c26b14055b90b952d1362cbee2436033e07e3918
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-28 17:42:17 +0530

    28-09-2024✅

---
+++ MaxConsecutiveOnes3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        res = 0
+        numZeros = 0
+
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                numZeros += 1
+
+            while numZeros > k:
+                if nums[l] == 0:
+                    numZeros -= 1
+                l += 1
+            res = max(res, (r-l+1))
+        
+        return res
