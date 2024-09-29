f4c2ed0c8286d12d5b72f07a5f28f83d2f502b6a
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-29 20:55:32 +0530

    29-09-2024✅

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -7,3 +7,20 @@ class Solution:
             if len(minHeap) > k:
                 heapq.heappop(minHeap)
         return [item[1] for item in minHeap]
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        freqVal = {}
+        for n in nums:
+            freqVal[n] = 1 + freqVal.get(n, 0)
+        
+        for key, val in freqVal.items():
+            heapq.heappush(heap, (-val, key))
+        
+        res = []
+        while len(res) < k:
+            res.append(heapq.heappop(heap)[1])
+        
+        return res
