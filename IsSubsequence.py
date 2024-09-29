1b1f264dc74987a44d667ea0bfa0aac09427c7eb
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-29 21:21:11 +0530

    29-09-2024✅

--- IsSubsequence.py
+++ IsSubsequence.py
@@ -7,3 +7,14 @@ class Solution:
         return i == len(s)
 
 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+            
+        return len(s) == i
