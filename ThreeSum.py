06064920a597726881da72b3cb8263990b8a18ba
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-29 21:37:37 +0530

    29-09-2024✅

--- ThreeSum.py
+++ ThreeSum.py
@@ -32,3 +32,31 @@ class Solution:
 # if sum>0 we decrement right pointer
 # if sum<0 we increment left pointer
 # if it equals to zero, then we append it in the result
+
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total > 0:
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    res.append([nums[i] , nums[left] , nums[right]])
+                    left += 1
+
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        return res
