ee4cfefaf084ef2db4410ccc46f3f4b5526131d8
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-30 10:04:41 +0530

    30-09-2024✅

--- IndexOfFristOccurence.py
+++ IndexOfFristOccurence.py
@@ -35,3 +35,15 @@ class Solution:
                     lps[i] = 0
                     i += 1
         return lps
+    
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack) < len(needle):
+            return -1
+        
+        for i in range(len(haystack)):
+            if haystack[i : i+(len(needle))] == needle:
+                return i
+        
+        return -1
