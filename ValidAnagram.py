0d65b5b7c68853a33a8f5bd7db7b5858c3278a38
Keyurrr14 <keyurrathod9920@gmail.com>
2024-09-30 10:07:30 +0530

    30-09-2024✅

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,3 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+    
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+
+        for i in s:
+            count[i] += 1
+        
+        for i in t:
+            count[i] -= 1
+
+        for val in count.values():
+            if val != 0:
+                return False
+            
+        return True
