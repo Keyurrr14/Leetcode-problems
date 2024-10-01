118c1c982466cb5786bf35d8015ceddb417cd9f1
Keyurrr14 <keyurrathod9920@gmail.com>
2024-10-01 09:32:58 +0530

    01-10-2024✅

--- DistributeCandies.py
+++ DistributeCandies.py
@@ -8,3 +8,8 @@ class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         return min(len(set(candyType)), len(candyType) // 2)
     
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        res1 = len(set(candyType))
+        res2 = len(candyType)
+        return res1 if res2/2 >= res1 else int(res2/2)
