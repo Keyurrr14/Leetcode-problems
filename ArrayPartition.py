38f370627c5db432f25802e87ecc7dd4df35a1d8
Keyurrr14 <keyurrathod9920@gmail.com>
2024-10-01 09:23:47 +0530

    01-10-2024✅

--- ArrayPartition.py
+++ ArrayPartition.py
@@ -2,3 +2,11 @@ class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         return sum(sorted(nums)[::2])
 
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+        for i in range(0, len(nums), 2):
+            res += nums[i]
+        
+        return res
