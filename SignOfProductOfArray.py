3e007e7481ecf7c20bdec96d59ddb508ba9d00fe
Keyurrr14 <keyurrathod9920@gmail.com>
2024-10-02 16:04:31 +0530

    02-10-2024✅

--- SignOfProductOfArray.py
+++ SignOfProductOfArray.py
@@ -13,3 +13,15 @@ class Solution:
                 return 0
         
         return signFunc(res)
+    
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        isNegative = False
+
+        for num in nums:
+            if num == 0:
+                return 0
+            elif num < 0:
+                isNegative = not isNegative
+        
+        return -1 if isNegative else 1
