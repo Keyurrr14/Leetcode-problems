e5294bf7c27a18c6cf0ace4a15628e8c6731447d
Keyurrr14 <keyurrathod9920@gmail.com>
2024-10-04 12:29:36 +0530

    04-10-2024✅

--- DateToBinary.py
+++ DateToBinary.py
@@ -1,3 +1,8 @@
 class Solution:
     def convertDateToBinary(self, date: str) -> str:
         return f"{int(date[0:4]):b}-{int(date[5:7]):b}-{int(date[8:10]):b}"
+    
+
+class Solution:
+    def convertDateToBinary(self, date: str) -> str:
+        return "-".join(f"{int(d):b}" for d in date.split("-"))
