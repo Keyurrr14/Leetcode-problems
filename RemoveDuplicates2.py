cfe2f2ff0b98f6e7ab336f967c866120a918ecc0
Keyurrr14 <keyurrathod9920@gmail.com>
2024-10-24 18:09:37 +0530

    24-10-2024✅

--- RemoveDuplicates2.py
+++ RemoveDuplicates2.py
@@ -11,3 +11,13 @@ class Solution:
                 left += 1
             right += 1
         return left
+    
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        del nums[j:]
+        return len(nums)
