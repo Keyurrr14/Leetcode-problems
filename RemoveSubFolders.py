103d93e2026f3e6071fa679fa85c2a9a637e4e92
Keyurrr14 <keyurrathod9920@gmail.com>
2024-10-25 13:05:48 +0530

    25-10-2024✅

--- RemoveSubFolders.py
+++ RemoveSubFolders.py
@@ -7,3 +7,16 @@ class Solution:
             if not folder[i].startswith(last):
                 res.append(folder[i])
         return res
+    
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folderSet = set(folder)
+        res = []
+
+        for f in folder:
+            res.append(f)
+            for i in range(len(f)):
+                if f[i] == '/' and f[:i] in folderSet:
+                    res.pop()
+                    break
+        return res
