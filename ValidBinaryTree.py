38839349b545fc05058f48e5af7417d94cd50225
Keyurrr14 <keyurrathod9920@gmail.com>
2024-11-07 19:37:22 +0530

    07-11-2024✅

--- ValidBinaryTree.py
+++ ValidBinaryTree.py
@@ -14,3 +14,22 @@ class Solution:
             return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
 
         return valid(root, float("-inf"), float("inf"))
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, minn, maxx):
+            if not node:
+                return True
+            
+            if node.val <= minn or node.val >= maxx:
+                return False
+
+            return valid(node.left, minn, node.val) and valid(node.right, node.val, maxx)
+
+        return valid(root, float("-inf"), float("inf")) 
