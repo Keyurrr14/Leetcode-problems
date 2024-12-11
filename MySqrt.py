299399c3eb3249fd474702346867ff36cf465617
Keyurrr14 <keyurrathod9920@gmail.com>
2024-12-11 14:20:17 +0530

    11-12-2024✅

--- MySqrt.py
+++ MySqrt.py
@@ -16,3 +16,18 @@ class Solution:
                 return mid
             
         return right
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 1, x
+
+        while left <= right:
+            mid = (left + right) // 2
+            midSquared = mid * mid
+            if midSquared == x:
+                return mid
+            elif midSquared < x:
+                left = mid  + 1
+            else:
+                right = mid - 1
+        return right
