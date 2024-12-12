535f9021e2e7843ba18dc801f5ecff651bc5c3a8
Keyurrr14 <keyurrathod9920@gmail.com>
2024-12-12 22:27:27 +0530

    12-12-2024✅

--- DesignHashSet.py
+++ DesignHashSet.py
@@ -15,4 +15,20 @@ class MyHashSet:
 
     def contains(self, key: int) -> bool:
         return key in self.arr
+    
+
+class MyHashSet:
+
+    def __init__(self):
+        self.mp=[False]*1000001
         
+
+    def add(self, key: int) -> None:
+        self.mp[key]=True
+        
+
+    def remove(self, key: int) -> None:
+        self.mp[key]=False
+
+    def contains(self, key: int) -> bool:
+        return self.mp[key]
